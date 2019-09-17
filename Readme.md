1c0e0faad1c4fd5f7aee2109a92cd6597ed09f61
peter macharia <mainapeter081@gmail.com>
2019-09-17 20:13:20 +0300

    extract the text in your image vesion 1.0

---
+++ Readme.md
@@ -0,0 +1,16 @@
+script name : imageText.py
+imageText.py will extract text from a given image
+The text will be saved in the file specified by user
+
+
+To run the program:
+make sure all the modules are installed on your system
+To install a module use pip3 install <module name>
+The script should be run on python3 and on a unix based system
+on the Terminal do the following
+python3 imageText.py 
+
+"The system will then guide you,Enter the filename to save the text
+and also specify the path to the image"
+
+
---
+++ imageText.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+from PIL import Image
+import pytesseract 
+import os
+import sys
+#we are going ti extract text from an image and write the text to a file
+
+#ask the user for the name of the file to write the text
+def fileName():
+	print("Name of the file to copy the text to")
+	file_name = input().strip()
+	if not file_name:
+		file_name = 'imageText'
+	return file_name	
+def imagePath():	
+	print("specify the path of the image to extract text from")
+	image_path = input()
+	if os.path.exists(image_path):
+		image_path = image_path
+	else:
+		print(f"The file {image_path} does not exist")
+		imagePath()
+	return image_path
+
+if __name__ == '__main__':
+	try:
+		file_name = fileName()
+
+		image = imagePath()
+		while os.path.exists(image) != True:
+			image = imagePath()
+
+		with open(file_name,"w") as f:
+			itx = pytesseract.image_to_string(image,lang='eng')
+			f.write(itx)
+	except KeyboardInterrupt:
+		print("---interrupted---")
+		sys.exit()
+	except Exception as e:
+		print("try again")
+		sys.exit()
